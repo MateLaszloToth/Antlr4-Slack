fc19982551a1c7b72f8b63029842b9b5a0db9fdd
matetoth <mate.toth@kambr.com>
2021-03-17 12:21:21 -0400

    Refactor constructors in Json dataClasses

--- src/main/java/com/kambr/parser/oney/json/dataClasses/legData/CabinDetails.kt
+++ src/main/java/com/kambr/parser/oney/json/dataClasses/legData/CabinDetails.kt
@@ -8,18 +8,69 @@ import com.kambr.parser.oney.json.dataClasses.legData.FieldNames.CABIN_CODE
 import com.kambr.parser.oney.json.dataClasses.legData.FieldNames.CABIN_SALEABLE_CAPACITY
 
 @Suppress("UNCHECKED_CAST")
-class CabinDetails(cabinObject: HashMap<String, Any>) {
-    var cabinCode: String = cabinObject[CABIN_CODE.value] as String
-    var cabinSaleableCapacity: Int = cabinObject[CABIN_SALEABLE_CAPACITY.value]!!.toInteger()
-    var cabinAuthorizedCapacity: Int = cabinObject[CABIN_AUTHORIZED_CAPACITY.value]!!.toInteger()
-    var cabinBlock: Int = cabinObject[CABIN_BLOCK.value]!!.toInteger()
-    var blockSpaceDetails: MutableList<BlockSpaceDetails> = mutableListOf()
+class CabinDetails {
+    val cabinCode: String
+    val cabinSaleableCapacity: Int
+    val cabinAuthorizedCapacity: Int
+    val cabinBlock: Int
+    val blockSpaceDetails: List<BlockSpaceDetails>
 
-    init {
+    constructor(
+        cabinObject: HashMap<String, Any>
+    ) {
+        cabinCode = cabinObject[CABIN_CODE.value] as String
+        cabinSaleableCapacity = cabinObject[CABIN_SALEABLE_CAPACITY.value]!!.toInteger()
+        cabinAuthorizedCapacity = cabinObject[CABIN_AUTHORIZED_CAPACITY.value]!!.toInteger()
+        cabinBlock = cabinObject[CABIN_BLOCK.value]!!.toInteger()
+
+        val tempBlockSpaceDetails = mutableListOf<BlockSpaceDetails>()
         (cabinObject[BLOCK_SPACE_DETAILS.value] as List<*>).forEach { item ->
             if (item != null) {
-                blockSpaceDetails.add(BlockSpaceDetails(item as HashMap<String, String>))
+                tempBlockSpaceDetails.add(BlockSpaceDetails(item as HashMap<String, String>))
             }
         }
+        blockSpaceDetails = tempBlockSpaceDetails
+    }
+
+    constructor(
+        cabinCode: String,
+        cabinSaleableCapacity: Int,
+        cabinAuthorizedCapacity: Int,
+        cabinBlock: Int,
+        blockSpaceDetails: List<BlockSpaceDetails>
+    ) {
+        this.cabinCode = cabinCode
+        this.cabinSaleableCapacity = cabinSaleableCapacity
+        this.cabinAuthorizedCapacity = cabinAuthorizedCapacity
+        this.cabinBlock = cabinBlock
+        this.blockSpaceDetails = blockSpaceDetails
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as CabinDetails
+
+        if (cabinCode != other.cabinCode) return false
+        if (cabinSaleableCapacity != other.cabinSaleableCapacity) return false
+        if (cabinAuthorizedCapacity != other.cabinAuthorizedCapacity) return false
+        if (cabinBlock != other.cabinBlock) return false
+        if (blockSpaceDetails != other.blockSpaceDetails) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = cabinCode.hashCode()
+        result = 31 * result + cabinSaleableCapacity
+        result = 31 * result + cabinAuthorizedCapacity
+        result = 31 * result + cabinBlock
+        result = 31 * result + blockSpaceDetails.hashCode()
+        return result
+    }
+
+    override fun toString(): String {
+        return "CabinDetails(cabinCode='$cabinCode', cabinSaleableCapacity=$cabinSaleableCapacity, cabinAuthorizedCapacity=$cabinAuthorizedCapacity, cabinBlock=$cabinBlock, blockSpaceDetails=$blockSpaceDetails)"
     }
 }
--- src/main/java/com/kambr/parser/oney/json/dataClasses/schedule/Schedule.kt
+++ src/main/java/com/kambr/parser/oney/json/dataClasses/schedule/Schedule.kt
@@ -4,15 +4,48 @@ import com.kambr.parser.oney.json.dataClasses.schedule.FiledNames.FLIGHT_KEY
 import com.kambr.parser.oney.json.dataClasses.schedule.FiledNames.FLIGHT_LEGS
 
 @Suppress("UNCHECKED_CAST")
-class Schedule(row: HashMap<String, Any>) {
-    val flightKey: FlightKey = FlightKey(row[FLIGHT_KEY.value]!! as HashMap<String, Any>)
-    val flightLegs: MutableList<FlightLeg> = mutableListOf()
+class Schedule {
+    val flightKey: FlightKey
+    val flightLegs: List<FlightLeg>
 
-    init {
+
+    constructor(row: HashMap<String, Any>) {
+        flightKey = FlightKey(row[FLIGHT_KEY.value]!! as HashMap<String, Any>)
+
+        val tempFlightLegs = mutableListOf<FlightLeg>()
         (row[FLIGHT_LEGS.value] as List<*>).forEach { flightLeg ->
-            if(flightLeg != null) {
-                flightLegs.add(FlightLeg(flightLeg as HashMap<String, Any>))
+            if (flightLeg != null) {
+                tempFlightLegs.add(FlightLeg(flightLeg as HashMap<String, Any>))
             }
         }
+
+        flightLegs = tempFlightLegs
+    }
+
+    constructor(flightKey: FlightKey, flightLegs: List<FlightLeg>) {
+        this.flightKey = flightKey
+        this.flightLegs = flightLegs
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Schedule
+
+        if (flightKey != other.flightKey) return false
+        if (flightLegs != other.flightLegs) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = flightKey.hashCode()
+        result = 31 * result + flightLegs.hashCode()
+        return result
+    }
+
+    override fun toString(): String {
+        return "Schedule(flightKey=$flightKey, flightLegs=$flightLegs)"
     }
 }
